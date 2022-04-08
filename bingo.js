8ab12dda08b629a7078a5b20c918cd482873b981
otaviolube <otaviolube@gmail.com>
2022-04-08 09:18:34 -0300

    Verficiar bingo

--- bingo.js
+++ bingo.js
@@ -1,26 +1,26 @@
-function gerarNumerosAleatorios(quantidade, min, max){
-    
-    if(max - min < quantidade){
+function gerarNumerosAleatorios(quantidade, min, max) {
+
+    if (max - min < quantidade) {
         console.log("Não existem números suficientes");
         return;
     }
-    
+
     let numerosAleatorios = [];
 
-    for(let i = 0; i < quantidade; i++){
-        let numero = Math.ceil(Math.random()*(max - min)) + min;
-        if(numerosAleatorios.indexOf(numero) !== -1){
+    for (let i = 0; i < quantidade; i++) {
+        let numero = Math.ceil(Math.random() * (max - min)) + min;
+        if (numerosAleatorios.indexOf(numero) !== -1) {
             i--;
-        }else{
+        } else {
             numerosAleatorios.push(numero);
         }
     }
 
     return numerosAleatorios;
-    
+
 }
 
-function gerarCartelaBingo(){
+function gerarCartelaBingo() {
     let area_cartela = document.getElementById("cartela");
     let cartela = document.createElement("table");
     let cabecalho_cartela = document.createElement("thead");
@@ -36,26 +36,26 @@ function gerarCartelaBingo(){
     coluna4.innerText = "G";
     let coluna5 = document.createElement("td");
     coluna5.innerText = "O";
-    
+
     linha.appendChild(coluna1);
     linha.appendChild(coluna2);
     linha.appendChild(coluna3);
     linha.appendChild(coluna4);
     linha.appendChild(coluna5);
-    
+
     cabecalho_cartela.appendChild(linha);
-    
+
     cartela.appendChild(cabecalho_cartela);
 
     let cartelaBingo = [];
-    cartelaBingo.push(gerarNumerosAleatorios(5,1,15));
-    cartelaBingo.push(gerarNumerosAleatorios(5,16,30));
-    cartelaBingo.push(gerarNumerosAleatorios(5,31,45));
-    cartelaBingo.push(gerarNumerosAleatorios(5,46,60));
-    cartelaBingo.push(gerarNumerosAleatorios(5,61,75));
+    cartelaBingo.push(gerarNumerosAleatorios(5, 1, 15));
+    cartelaBingo.push(gerarNumerosAleatorios(5, 16, 30));
+    cartelaBingo.push(gerarNumerosAleatorios(5, 31, 45));
+    cartelaBingo.push(gerarNumerosAleatorios(5, 46, 60));
+    cartelaBingo.push(gerarNumerosAleatorios(5, 61, 75));
 
     console.log(cartelaBingo);
-    
+
     for (let i = 0; i < 5; i++) {
         let linha_dados = document.createElement("tr");
         for (let j = 0; j < 5; j++) {
@@ -65,28 +65,28 @@ function gerarCartelaBingo(){
         }
         corpo_cartela.appendChild(linha_dados);
     }
-    
+
     cartela.appendChild(corpo_cartela);
     area_cartela.appendChild(cartela);
 }
 
-function deletarCartelas(){
+function deletarCartelas() {
 
     let cartelas = document.getElementsByTagName("table");
 
     console.log(cartelas.length)
 
-    for(let i = 0; i < cartelas.length; i++){
+    for (let i = 0; i < cartelas.length; i++) {
         cartelas[i].remove();
     }
 
 }
 
-function sorteio(){
+function sorteio() {
 
     let cartelas = document.getElementsByTagName("table");
 
-    if(cartelas.length === 0){
+    if (cartelas.length === 0) {
         alert("Você precisa criar uma cartela antes!");
         return;
     }
@@ -95,26 +95,51 @@ function sorteio(){
 
     let numerosSorteados = []
 
-    let intervalo = setInterval(function(){
-        console.log(numerosSorteados)
-        let aleatorio = Math.ceil(Math.random()*75);
-        if(numerosSorteados.indexOf(aleatorio) === -1){
-            numerosSorteados.push(aleatorio);
-            let numero = document.createElement("span");
-            numero.innerText = aleatorio;
-            divsorteados.appendChild(numero);
-            //Conferir as cartelas
-            for(let i = 0; i < cartelas.length; i++){
-                let numerosCartela = cartelas[i].getElementsByTagName("td");
-                for(let j = 0; j < numerosCartela.length; j++){
-                    if(numerosCartela[j].innerText == aleatorio){
-                        numerosCartela[j].style.backgroundColor = "green";
-                    }
+    let intervalo = setInterval(function () {
+        let aleatorio = 0;
+        do {
+            aleatorio = Math.ceil(Math.random() * 75);
+        } while (numerosSorteados.indexOf(aleatorio) !== -1)
+
+        numerosSorteados.push(aleatorio);
+        let numero = document.createElement("span");
+        numero.innerText = aleatorio;
+        divsorteados.appendChild(numero);
+        //Conferir as cartelas
+        for (let i = 0; i < cartelas.length; i++) {
+            let numerosCartela = cartelas[i].getElementsByTagName("td");
+            for (let j = 0; j < numerosCartela.length; j++) {
+                if (numerosCartela[j].innerText == aleatorio) {
+                    numerosCartela[j].style.backgroundColor = "green";
                 }
+            }
+
+            if (verificarVencedor(numerosCartela, numerosSorteados)) {
+                alert("Parabéns! Você ganhou o bingo!");
+                clearInterval(intervalo);
             }
         }
 
-        if(numerosSorteados.length === 75) clearInterval(intervalo);
-    }, 500)
 
+        if (numerosSorteados.length === 75) clearInterval(intervalo);
+    }, 200)
+
+}
+
+function verificarVencedor(cartela, numerosSorteados) {
+    if (numerosSorteados.length < 25) return;
+
+    cartela = [...cartela];
+
+    cartela.splice(0,5);
+
+    for (let i = 0; i < cartela.length; i++) {
+        console.log(numerosSorteados);
+        console.log(cartela[i].innerText)
+        if(numerosSorteados.indexOf(parseInt(cartela[i].innerText)) == -1){
+            return false;
+        }
+    }
+
+    return true;
 }
